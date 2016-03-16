abd3837e265a3c3b6144f255fb84fbe69e03a93d
Matt Muckle <matt.muckle@brightspec.com>
2016-03-16 13:38:48 -0400

    fixed labels to mhz

--- app/forms.py
+++ app/forms.py
@@ -11,8 +11,8 @@ from wtforms.validators import DataRequired
 
 
 class SpectraForm(Form):
-    start_freq = IntegerField('Start Frequency (GHz):', default=2000)
-    stop_freq = IntegerField('Stop Frequency (GHz):', default=600000)
+    start_freq = IntegerField('Start Frequency (MHz):', default=2000)
+    stop_freq = IntegerField('Stop Frequency (MHz):', default=600000)
     temp = IntegerField('Temperature (K):', default=300)
     molecules = SelectMultipleField("Molecules:", coerce=int, choices=[(0, "Inactive"), (1, "Active")],
                                     default=1 ,
